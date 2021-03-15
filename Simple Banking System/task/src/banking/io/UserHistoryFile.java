3527f960c86529c66c497e5465c65796c04577d0
Sergey Zhirkov <zhirkov_55250@mail.ru>
2021-03-15 18:36:02 +0500

    add ExecuteService to user's history files for use in parallel blocking-io-write operations

--- Simple Banking System/task/src/banking/io/UserHistoryFile.java
+++ Simple Banking System/task/src/banking/io/UserHistoryFile.java
@@ -7,16 +7,19 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class UserHistoryFile {
     private File uFile;
-    private static final String localPath = "./user_history_files/";
-    private static final String prefix = "bank_history_user_id# ";
-    private static final String ext = ".txt";
+    private static final String LOCAL_PATH = "./user_history_files/";
+    private static final String PREFIX = "bank_history_user_id# ";
+    private static final String EXT = ".txt";
+    private final ExecutorService fileThreadService = Executors.newSingleThreadExecutor();
 
     public UserHistoryFile(Account account) {
         Card userCard = account.getCard();
-        uFile = new File(localPath + prefix + userCard.getNumber() + ext);
+        uFile = new File(LOCAL_PATH + PREFIX + userCard.getNumber() + EXT);
         if (!uFile.exists()) {
             try {
                 uFile.createNewFile();
@@ -31,17 +34,22 @@ public class UserHistoryFile {
     }
 
     public void printInfo(String info) {
-        try (PrintStream ps = new PrintStream(new FileOutputStream(uFile, true))) {
-            ps.println("\n" + info);
-        } catch (IOException e) {
-            System.out.println("file exception!");
-        }
-
+        fileThreadService.execute(() -> {
+            try (PrintStream ps = new PrintStream(new FileOutputStream(uFile, true))) {
+                ps.println("\n" + info);
+            } catch (IOException e) {
+                System.out.println("file exception!");
+            }
+        });
     }
 
     public void printMessage(String message) {
         printInfo(message);
         System.out.println(message);
+    }
+
+    public void closeExecuteService() {
+        fileThreadService.shutdown();
     }
 
 }
